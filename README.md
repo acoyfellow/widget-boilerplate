8b778b85d012aaa5c7d2bc8fb79928cb77a3afb4
Jordan Coeyman <coeyman@gmail.com>
2018-11-21 13:53:23 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# widget-boilerplate
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import "tachyons/css/tachyons.min.css";
+import "./style";
+import { Component, render } from "preact";
+import { Result } from "./result";
+
+const SEARCH = "//api.github.com/search/repositories";
+
+export default class App extends Component {
+  componentDidMount() {
+    fetch(`${SEARCH}?q=preact`)
+      .then(r => r.json())
+      .then(json => {
+        this.setState({
+          results: (json && json.items) || []
+        });
+      });
+  }
+
+  render(props, { results = [] }) {
+    return (
+      <div>
+        <h1>Example</h1>
+        <div class="list bg-gray pa3">
+          {results.map(result => (
+            <Result result={result} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+if (typeof window !== "undefined") {
+  render(<App />, document.getElementById("root"));
+}
